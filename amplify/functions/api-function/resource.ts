c5f63739b845c4296a0ca59dd5ed2cef3690e3aa
andrii.logoshko <forando@gmail.com>
2024-05-14 16:07:36 +0200

    Use ENV For APP_ID

--- amplify/functions/api-function/resource.ts
+++ amplify/functions/api-function/resource.ts
@@ -1,4 +1,4 @@
-import { defineFunction, secret } from "@aws-amplify/backend";
+import { defineFunction } from "@aws-amplify/backend";
 import {CfnFunction, IFunction} from "aws-cdk-lib/aws-lambda";
 import * as iam from "aws-cdk-lib/aws-iam"
 import { Stack } from "aws-cdk-lib";
@@ -6,9 +6,6 @@ import { Stack } from "aws-cdk-lib";
 export const apiFunction = defineFunction({
     name: "api-function",
     entry: "src/handler.ts",
-    environment: {
-        APP_ID: secret('APP_ID')
-    }
 });
 
 export const configureDatabaseEnvForFunction = (lambda: CfnFunction, tableName: string) => {
@@ -23,7 +20,7 @@ export const grantSSMAccess = (lambda: IFunction, stack: Stack) => {
             "ssm:PutParameter",
             "ssm:GetParametersByPath"
         ],
-        resources: [`arn:aws:ssm:${stack.region}:${stack.account}:parameter/amplify/shared/${secret('APP_ID')}`],
+        resources: [`arn:aws:ssm:${stack.region}:${stack.account}:parameter/amplify/shared/${process.env.APP_ID}`],
     });
 
 
