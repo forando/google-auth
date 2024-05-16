49ce484b528e36374bc67616e7d8599602581243
andrii.logoshko <forando@gmail.com>
2024-05-16 21:02:22 +0200

    Configure ServiceWorker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,62 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        srcDir: 'service-worker',
+        strategies: 'injectManifest',
+        filename: 'sw.ts',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        manifest: {
+          name: 'Google Auth',
+          short_name: 'Google Auth',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
+          ],
+        },
+        workbox: {
+          cleanupOutdatedCaches: true,
+          clientsClaim: true,
+          skipWaiting: true,
+        },
+        injectManifest: {
+          minify: false,
+          enableWorkboxModulesLogs: true,
+        },
+        devOptions: {
+          enabled: process.env.SW_DEV === 'true',
+          /* when using generateSW the PWA plugin will switch to classic */
+          navigateFallback: 'index.html',
+          suppressWarnings: true,
+          type: 'module',
+        },
+      })
+  ],
 })
